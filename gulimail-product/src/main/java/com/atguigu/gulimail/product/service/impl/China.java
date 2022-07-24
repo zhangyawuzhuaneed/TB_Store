f4a2b346a89a896ff8674d87ff74ec268eca3ae1
Zhang <19861827467@163.com>
2022-07-24 15:44:51 +0800

    update

---
+++ gulimail-product/src/main/java/com/atguigu/gulimail/product/service/impl/China.java
@@ -0,0 +1,14 @@
+package com.atguigu.gulimail.product.service.impl;
+
+
+
+@Data
+public class China {
+
+    private int id;
+    private String Name;
+
+    public static void main(String[] args) {
+    }
+
+}
